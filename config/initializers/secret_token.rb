0b9aa27a6dc343b51d8e766ca3c140f684097b39
Tim Garcia <tim.m.garcia@gmail.com>
2014-01-28 09:58:07 -0800

    Adding the initial check in of the store app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '3bf9632b449ab7cf60d9a86c7738b2b3c8e38de45ffd3a13228a14cc3310b9dbb5be8e8c37821a074a004ae6786b3399d423bfe33d937c8e248959f1204d6dc3'
